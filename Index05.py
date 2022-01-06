f4c868ce44005cf527d878fb6c6cba43e8f73d38
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-06 21:55:51 +0500

    ndx05-1

--- Index05.py
+++ Index05.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +7,18 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    
+    m=0
+    if '\060'<=s[-1]<='\070':
+        m+=1
+    if '\060'<=s[-2]<='\070':
+        m+=1
+    if '\060'<=s[-3]<='\070':
+        m+=1
+    if '\060'<=s[-4]<='\070':
+        m+=1
+    if '\060'<=s[-5]<='\070':
+        m+=1
+    return m
+print(main("1a2s3"))
+
