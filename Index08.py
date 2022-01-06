cb90e8f6314a14ac901417074da7747e73c297e6
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-06 21:57:56 +0500

    ndx08-1

--- Index08.py
+++ Index08.py
@@ -6,5 +6,16 @@ def main(s):
     Returns:
         int: answer
     """
-    return
-        
+    m=0
+    if s[-1]=='\052':
+        m+=1
+    if s[-2]=='\052':
+        m+=1
+    if s[-3]=='\052':
+        m+=1
+    if s[-4]=='\052':
+        m+=1    
+    if s[-5]=='\052':
+        m+=1    
+    return m
+print(main("go*od"))
